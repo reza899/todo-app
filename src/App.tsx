e133ba4641fe0d7f34736d24a44f4905b7f3e507
Reza <reza.bozorgi@outlook.com>
2021-09-30 20:47:02 +0330

    add logo and UI improvement

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Todo from "./components/Todo";
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
+    <div className="flex justify-center items-start  bg-gray-100 h-screen overflow-y-auto">
       <Todo />
     </div>
   );
--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -8,19 +8,22 @@ interface Props {
 
 const AddTodo = ({ task, onSubmit, onChnage }: Props) => {
   return (
-    <form className="flex justify-center w-full" onSubmit={onSubmit}>
-      <input
-        className=" rounded shadow p-2 text-gray-500 mr-2 placeholder-gray-200"
-        type="text"
-        name="task"
-        value={task}
-        onChange={onChnage}
-        placeholder="do ..."
-      />
-      <button className="shadow rounded text-gray-600 px-3" type="submit">
-        Add
-      </button>
-    </form>
+    <>
+      <img src="todo.png" alt="todo logo" width={150} />
+      <form className="flex justify-center w-full" onSubmit={onSubmit}>
+        <input
+          className="w-full rounded shadow p-2 text-gray-500 mr-2 placeholder-gray-200"
+          type="text"
+          name="task"
+          value={task}
+          onChange={onChnage}
+          placeholder="do ..."
+        />
+        <button className="shadow rounded text-gray-600 px-3" type="submit">
+          Add
+        </button>
+      </form>
+    </>
   );
 };
 
